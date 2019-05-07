c8ae86bc5b31ea8bdb21911e86a301271b8c8f4c
Damian Cosmelli <damiancosmelli@hotmail.com>
2019-05-06 21:34:47 -0300

    Inicio del Repositorio

---
+++ myFirstScrapy/spiders/jobs.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from scrapy.linkextractors import LinkExtractor
+from scrapy.selector import Selector
+from scrapy.spiders import CrawlSpider
+from scrapy.spiders import Rule
+from myFirstScrapy.items import *
+
+class JobsSpider(CrawlSpider):
+    name = 'jobs'
+    allowed_domains = ['www.python.org']
+    start_urls = ['http://www.python.org/',
+                  'https://www.python.org/jobs/']
+
+    rules = (
+        Rule(LinkExtractor(allow=(), restrict_css=('.list-recent-jobs',)),
+             callback="parse_item",
+             follow=True),)
+
+    def parse_item(self, response):
+        if not str(response.url).__contains__('location'):
+            itemlink = str(response.css('.text > .listing-company > .listing-company-name > .company-name::text').extract()).replace('\\n\\t', ' ').replace('\\t', ' ').replace('\\n', ' ').replace(',', ' ').strip()
+
+            item = MyfirstscrapyItem()
+            item['puesto'] = itemlink
+            item['url'] = str(response.url)
+            yield item
